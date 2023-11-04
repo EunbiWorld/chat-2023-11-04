39693d7a4235d0c856f67575f0e4764fada0b6e3
EunbiWorld <rkql1304@naver.com>
2023-11-04 14:49:26 +0900

    ChatRoomService, ChatRoomRepositort

--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -1,12 +1,20 @@
 package com.ll.chat20231104.domain.chat.chatRoom.controller;
 
+import com.ll.chat20231104.domain.chat.chatRoom.entity.ChatRoom;
+import com.ll.chat20231104.domain.chat.chatRoom.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // ChatController가 컨트롤러
 @Controller
 @RequestMapping("/chat/room")
+@RequiredArgsConstructor
 public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+
     @GetMapping("/{roomId}") // get 방식의 통신
     @ResponseBody // 메소드 실행
     public String showRoom(
@@ -19,6 +27,20 @@ public class ChatRoomController {
     @GetMapping("/make") // get 방식의 통신
     public String showMake() {
         return "domain/chat/chatRoom/make";
+    }
+
+    @PostMapping("/make")
+    public String make(
+            final String name
+    ) {
+        chatRoomService.make(name);
+        return "redirect:/chat/room/make?message=Chat Room Created";
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<ChatRoom> showList() {
+        return chatRoomService.findAll();
     }
 }
 
