5aac9b13d7df79172414f18f1b6ff4d566b22420
EunbiWorld <rkql1304@naver.com>
2023-11-04 14:14:00 +0900

    RequestMapping

--- src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chat20231104/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -1,21 +1,34 @@
 package com.ll.chat20231104.domain.chat.chatRoom.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 // ChatController가 컨트롤러
 @Controller
+@RequestMapping("/chat/room")
 public class ChatRoomController {
-    @GetMapping("/chat/room/{roomId}") // get 방식의 통신
+    @GetMapping("/{roomId}") // get 방식의 통신
     @ResponseBody // 메소드 실행
     public String showRoom(
         @PathVariable final long roomId,
         @RequestParam(defaultValue = "NoName") final String writerName
     ) {
         return "%d번 채팅방입니다. writer : %s".formatted(roomId, writerName);
+    }
+
+    @GetMapping("/make") // get 방식의 통신
+    @ResponseBody // 메소드 실행
+    public String showMake() {
+        return """
+                <form action="">
+                    <div>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <input type="submit">
+                    </div>
+                </form>
+                """;
     }
 }
 
